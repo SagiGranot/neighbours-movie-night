e0cb20469a00cbd91cc6255c4248cc4c4dc29252
Sagi Granot <sagigranot@Sagis-MBP.lan>
2024-02-09 23:45:12 +0200

    create the movies store and service

---
+++ src/data/services/movies-service.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const baseURL = 'https://jsonmock.hackerrank.com/api/movies/search/';
+
+export const getMovies = async (params) => {
+    return await axios.get(baseURL, params);
+}
